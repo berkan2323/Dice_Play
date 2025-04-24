338bb07456daf8091925f3579e0d0175b02479da
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-24 23:33:57 +0300

    Add files via upload

---
+++ DicePlay/DicePlay/DicePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicePlayApp.swift
+//  DicePlay
+//
+//  Created by Berkan Aydın on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DicePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
